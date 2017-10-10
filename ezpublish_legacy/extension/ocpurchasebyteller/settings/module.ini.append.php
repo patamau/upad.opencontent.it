36f938f3372e18da7231d0dab13c6fb02242b75c
Matteo Pedrotti <matteo.pedrotti@deltainformatica.eu>
2017-10-10 11:57:56 +0200

    Aggiunto modulo cards per abilitare accesso al ruolo Sportellista

--- ezpublish_legacy/extension/ocpurchasebyteller/settings/module.ini.append.php
+++ ezpublish_legacy/extension/ocpurchasebyteller/settings/module.ini.append.php
@@ -5,6 +5,7 @@ ExtensionRepositories[]=ocpurchasebyteller
 ModuleList[]=ocpurchasebyteller
 ModuleList[]=ocorder
 ModuleList[]=courses
+ModuleList[]=cards
 ModuleList[]=export
 ModuleList[]=invoice
 ModuleList[]=utenti
